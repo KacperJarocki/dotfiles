3554e3cd7ed96092dae93bbadec687d355843439
Kacper Jarocki <incepcja@pop-os.localdomain>
2024-06-26 23:35:12 +0200

    working debuging

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,24 @@
 return {
   {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    "mfussenegger/nvim-dap",
+    dependencies = { "nvim-neotest/nvim-nio", "rcarriga/nvim-dap-ui" },
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      require("dapui").setup()
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
   },
 
   {
